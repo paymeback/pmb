412fadda0820ac05ab5c88118c9566717f6768d0
Pedalo <mail@schneid-media.de>
2013-02-16 02:46:50 +0100

    added model exchange

--- app/controllers/usdollar_controller.rb
+++ app/controllers/usdollar_controller.rb
@@ -5,8 +5,7 @@ require 'open-uri'
 	def index
 
 	doc = Nokogiri::XML(open("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"))
-
-	@dollar = doc.xpath('//Cube[currency="USD"]/rate').text
-
+	@dollar = doc.xpath("//ecb:Cube[@currency='USD']/@rate", 'ecb' => "http://www.ecb.int/vocabulary/2002-08-01/eurofxref" )
+	
 	end
-end
+end		

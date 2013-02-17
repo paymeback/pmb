5f0467092bded6c61ba9df89417d5ee1e93c8463
cith <thiel-chr@web.de>
2013-02-17 11:41:29 +0100

    created admin user, link to main page in admin dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Exchange.create(name: "USD", value: 3.3)
+
+#Create AdminUser
+u = User.create({"email" => "admin@pmb.dev","password" => "adminpmb","password_confirmation" => "adminpmb","nickname" => "admin"})
+u.add_role :admin
+u.save
+
+#Create Users
+3.times do |i|
+
+	
+end

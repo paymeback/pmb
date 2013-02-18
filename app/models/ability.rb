db5259062b1706b8bbcf312613a17374cafe459f
cith <thiel-chr@web.de>
2013-02-18 16:26:00 +0100

    added rspec and little code clean up

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,7 +13,7 @@ class Ability
 					can :update, Debt, :debitor_id => user.id
 					can :update, Debt, :creditor_id => user.id
 					can :get_file, Debt, :debitor_id => user.id
-					can :get_file, Debt, :creditor_id => user.id
+					can :get_file, Debt, :creditor_id => user.id				
 				end
 		else
 				can :index, Debt

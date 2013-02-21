1b02824d54e7e0d91f67c052c7245ec359067bca
Pedalo <mail@schneid-media.de>
2013-02-21 15:01:13 +0100

    code clean

--- app/controllers/usdollar.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-
-doc = Nokogiri::XML(File.open('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'))
-
-@dollar = doc.xpath('//Cube[currency='USD']/rate').each do |e|
-
-end
--- app/helpers/usdollar_helper.rb
+++
@@ -1,2 +0,0 @@
-module UsdollarHelper
-end
